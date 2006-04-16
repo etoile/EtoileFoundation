1705c341ba27157b2a754e2e31bc2bafa28fca8b
Quentin Mathé <quentin.mathe@gmail.com>
2006-04-16 10:25:35 +0000

    Ported Xcode project and integration from UnitKit 1.1 version (UnitKit project seems to be no longer maintained on Mac OS X)
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/UnitKit@724 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Source/XcodeIntegration/FileTemplates/class.h
@@ -0,0 +1,16 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright (c) «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UnitKit/UnitKit.h>
+
+@interface «FILEBASENAMEASIDENTIFIER» : NSObject <UKTest> {
+
+}
+
+@end
---
+++ Source/XcodeIntegration/FileTemplates/class.m
@@ -0,0 +1,14 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright (c) «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+#import "«FILEBASENAME».h"
+
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+@end
