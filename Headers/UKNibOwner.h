3dfe8d7e8982a938c69639efb8ca805dfcf95dfb
Günther Noack <guenther+github@unix-ag.uni-kl.de>
2006-08-17 20:14:40 +0000

    * Converted UKNibOwner documentation to doxygen syntax * Added -loadNib and -bundle methods * Made the class load Nibs from the bundle where the concrete subclass comes from
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileExtensionsKit@917 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/UKNibOwner.h
+++ Headers/UKNibOwner.h
@@ -2,14 +2,17 @@
 	FILE:		UKNibOwner.h
 	PROJECT:	CocoaTADS
 
-    COPYRIGHT:  (c) 2004 M. Uli Kusterer, all rights reserved.
+    COPYRIGHT:  (c) 2004-2006 M. Uli Kusterer, all rights reserved.
     
 	AUTHORS:	M. Uli Kusterer - UK
+                Guenther Noack - GN
     
     LICENSES:   GPL, Modified BSD
 
 	REVISIONS:
 		2004-11-13	UK	Created.
+        2006-08-17  GN  Added -bundle method.
+        2006-08-18  GN  Added -loadNib method, removed topLevelObjects field.
    ========================================================================== */
 
 // -----------------------------------------------------------------------------
@@ -25,9 +28,13 @@
 
 @interface UKNibOwner : NSObject
 {
-    NSMutableArray*     topLevelObjects;
+    BOOL nibLoaded;
 }
 
--(NSString*)    nibFilename;    // Defaults to name of the class.
+-(NSString*) nibFilename; // Defaults to name of the class.
+
+-(NSBundle*) bundle; // Defaults to the bundle of the concrete UKNibOwner (sub)class.
+
+-(BOOL) loadNib; // Loads Nib and returns YES on success.
 
 @end
