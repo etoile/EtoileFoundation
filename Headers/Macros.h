bbad9f03f7ade0511625daef677c9cd15cb92909
David Chisnall <github@theravensnest.org>
2007-09-22 22:41:29 +0000

    Added useful macros to EtoileFoundation.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@2479 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Headers/Macros.h
@@ -0,0 +1,33 @@
+/*
+ *  Macros.h
+ *
+ *  Created by David Chisnall on 02/08/2005.
+ *
+ */
+
+/**
+ * Simple macro for safely initialising the superclass.
+ */
+#define SUPERINIT if((self = [super init]) == nil) {return nil;}
+/**
+ * Simple macro for safely initialising the current class.
+ */
+#define SELFINIT if((self = [self init]) == nil) {return nil;}
+
+/**
+ * Set of macros providing a for each statement on collections, with IMP
+ * caching.
+ */
+#define FOREACHI(collection,object) FOREACH(collection,object,id)
+
+#define FOREACH(collection,object,type) FOREACHE(collection,object,type,object ## enumerator)
+
+#define FOREACHE(collection,object,type,enumerator)\
+NSEnumerator * enumerator = [collection objectEnumerator];\
+type object;\
+IMP next ## object ## in ## enumerator = \
+[enumerator methodForSelector:@selector(nextObject)];\
+while(enumerator != nil && (object = next ## object ## in ## enumerator(\
+												   enumerator,\
+												   @selector(nextObject))))
+
