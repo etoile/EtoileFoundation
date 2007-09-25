7cd2167f1cb8b71d0f61a9b9d5d33ce4eaa4d6f0
David Chisnall <github@theravensnest.org>
2007-09-25 11:00:28 +0000

    Added the files that I meant to add with the last commit.
    Thanks rburns for spotting this one...
    
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@2484 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Headers/NSArray+map.h
@@ -0,0 +1,5 @@
+#include <Foundation/Foundation.h>
+
+@interface NSArray (AllElements)
+- (id) map;
+@end
---
+++ Source/NSArray+map.m
@@ -0,0 +1,54 @@
+#import "NSArray+Map.h"
+#import "Macros.h"
+
+@interface NSArrayMapProxy : NSObject {
+	NSArray * array;
+}
+- (id) initWithArray:(NSArray*)anArray;
+@end
+
+@implementation NSArrayMapProxy
+- (id) initWithArray:(NSArray*)anArray
+{
+	SELFINIT;
+	array = [anArray retain];
+	return self;
+}
+- (id) methodSignatureForSelector:(SEL)aSelector
+{
+	FOREACHI(array, object)
+	{
+		if([object respondsToSelector:aSelector])
+		{
+			return [object methodSignatureForSelector:aSelector];
+		}
+	}
+	return [super methodSignatureForSelector:aSelector];
+}
+- (void) forwardInvocation:(NSInvocation*)anInvocation
+{
+	SEL selector = [anInvocation selector];
+	NSMutableArray * mappedArray = [NSMutableArray array];
+	FOREACHI(array, object)
+	{
+		if([object respondsToSelector:selector])
+		{
+			[anInvocation invokeWithTarget:object];
+			id mapped;
+			[anInvocation getReturnValue:&mapped];
+			[mappedArray addObject:mapped];
+		}
+	}
+	[anInvocation setReturnValue:mappedArray];
+}
+DEALLOC(
+	[array release];
+)
+@end
+
+@implementation NSArray (AllElements)
+- (id) map
+{
+	return [[[NSArrayMapProxy alloc] initWithArray:self] autorelease];
+}
+@end
