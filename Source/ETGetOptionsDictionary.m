4e5cece40b0b3c66d6d80b1bd6df0c6133948ae5
David Chisnall <github@theravensnest.org>
2008-06-10 10:18:30 +0000

    Changes based on qmathe's code review.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@3164 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/ETGetOptionsDictionary.m
+++ Source/ETGetOptionsDictionary.m
@@ -1,21 +1,21 @@
-#include <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
 #include <string.h>
 #include <unistd.h>
 
 NSDictionary *ETGetOptionsDictionary(char *optString, int argc, char **argv)
 {
 	NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-	NSNumber *True = [NSNumber numberWithBool:YES];
-	NSNumber *False = [NSNumber numberWithBool:NO];
-	for(char *opts = optString ; *opts ; opts++)
+	NSNumber *True = [NSNumber numberWithBool: YES];
+	NSNumber *False = [NSNumber numberWithBool: NO];
+	for(char *opts = optString ; '\0' != *opts ; opts++)
 	{
 		// Initialise options to False.
 		if (*opts != ':')
 		{
 			unichar opt = (unichar)*opts;
-			NSString *key = [NSString stringWithCharacters:&opt length:1];
-			[dict setObject:False
-			         forKey:key];
+			NSString *key = [NSString stringWithCharacters: &opt length: 1];
+			[dict setObject: False
+			         forKey: key];
 		}
 	}
 
@@ -24,21 +24,21 @@ NSDictionary *ETGetOptionsDictionary(char *optString, int argc, char **argv)
 	{
 		if (ch == '?')
 		{
-			[NSException raise:@"InvalidOption"
-			            format:@"Illegal argument %c", optopt];
+			[NSException raise: @"InvalidOption"
+			            format: @"Illegal argument %c", optopt];
 		}
 		unichar optchar = ch;
-		NSString *key = [NSString stringWithCharacters:&optchar length:1];
+		NSString *key = [NSString stringWithCharacters: &optchar length: 1];
 		char *opt = strchr(optString, (char)ch);
 		if (*(opt+1) == ':')
 		{
-			[dict setObject:[NSString stringWithUTF8String:optarg]
-			         forKey:key];
+			[dict setObject: [NSString stringWithUTF8String: optarg]
+			         forKey: key];
 		}
 		else
 		{
-			[dict setObject:True
-			         forKey:key];
+			[dict setObject: True
+			         forKey: key];
 		}
 	}
 	return dict;
