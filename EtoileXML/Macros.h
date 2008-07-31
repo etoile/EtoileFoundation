624560f423107e192ec6e5630960d97093eee26b
Quentin Mathé <quentin.mathe@gmail.com>
2008-07-31 16:29:30 +0000

    Cleaned EtoileXML Macros.h to only declare new macros and import all others from EtoileFoundation Macros.h
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@3433 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- EtoileXML/Macros.h
+++ EtoileXML/Macros.h
@@ -7,30 +7,7 @@
  *
  */
 
-#define SUPERINIT if((self = [super init]) == nil) {return nil;}
-#define SELFINIT if((self = [self init]) == nil) {return nil;}
-
-#define FOREACHI(collection,object) FOREACH(collection,object,id)
-
-#define FOREACH(collection,object,type) FOREACHE(collection,object,type,object ## enumerator)
-/*
-#define FOREACHE(collection,object,type,enumerator)\
-NSEnumerator * enumerator = [collection objectEnumerator];\
-type object;\
-while((object = [enumerator	nextObject]))
-*/
+#import "../Headers/Macros.h"
 
 #define AUTORELEASED(x) [[[x alloc] init] autorelease]
 #define RETAINED(x) [[x alloc] init]
-
-#define FOREACHE(collection,object,type,enumerator)\
-NSEnumerator * enumerator = [collection objectEnumerator];\
-type object;\
-IMP next ## object ## in ## enumerator = \
-[enumerator methodForSelector:@selector(nextObject)];\
-while(enumerator != nil && (object = next ## object ## in ## enumerator(\
-												   enumerator,\
-												   @selector(nextObject))))
-
-#define D(...) [NSDictionary dictionaryWithObjectsAndKeys:__VA_ARGS__ , nil]
-#define A(...) [NSArray arrayWithObjects:__VA_ARGS__ , nil]
