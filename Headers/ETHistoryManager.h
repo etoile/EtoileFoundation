c3bec113e349ddbcba4b5fb4e9c570eb5303443e
Truls Becken <truls.becken@gmail.com>
2008-12-14 21:34:26 +0000

    Checking in ETHistoryManager, but not adding it to the makefiles yet.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@4131 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ Headers/ETHistoryManager.h
@@ -0,0 +1,107 @@
+/*
+   ETHistoryManager.h
+
+   Copyright (C) 2008 Truls Becken <truls.becken@gmail.com>
+ 
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#import <Foundation/NSArray.h>
+#import <Foundation/NSEnumerator.h>
+
+/**
+ * ETHistoryManager keeps a history of objects of some kind. After going back
+ * in time, it can go forward again towards the most recent object. Adding an
+ * object while at a historic point will discard the forward history.
+ *
+ * It is also possible to give the manager an NSEnumerator to use as a lazy
+ * source for the forward history. This way, a collection of objects can be
+ * added as a "future", replacing the current forward history.
+ */
+@interface ETHistoryManager : NSObject
+{
+	NSMutableArray *history;
+	NSEnumerator *future;
+	int max_size;
+	int index;
+}
+
+/**
+ * Initialize the history manager.
+ */
+- (ETHistoryManager *) init;
+/**
+ * Set new current object, discarding the forward history.
+ */
+- (void) addObject: (id)object;
+/**
+ * Return the current object.
+ */
+- (id) currentObject;
+/**
+ * Advance the history one step forward if possible.
+ */
+- (void) next;
+/**
+ * Go one step back if possible.
+ */
+- (void) previous;
+/**
+ * Return YES if it is possible to go forward.
+ */
+- (BOOL) hasNext;
+/**
+ * Return YES if it is possible to go back.
+ */
+- (BOOL) hasPrevious;
+/**
+ * Return an object at a position relative to the current object. Return nil if
+ * the index refers to a point before the beginning or after the end of time.
+ */
+- (id) peek: (int)relativeIndex;
+/**
+ * Forget the history and discard the future.
+ */
+- (void) clear;
+/**
+ * Set an enumerator to use as the forward history, discarding everything after
+ * the current object.
+ */
+- (void) setFuture: (NSEnumerator *)enumerator;
+/**
+ * Set the maximum number of objects to remember. When more objects than this
+ * are added, the oldest ones are forgotten.
+ *
+ * The default is 100. Set it to 0 to remember an unlimited number of objects.
+ *
+ * Note that max size only limits the number of objects before currentObject.
+ * Setting a future and peeking deeply into it will force the history manager
+ * to temporarily hold more objects.
+ */
+- (void) setMaxHistorySize: (int)maxSize;
+/**
+ * Return the maximum number of objects to remember.
+ */
+- (int) maxHistorySize;
+/**
+ * Free resources held by the history manager.
+ */
+- (void) dealloc;
+
+@end
---
+++ Source/ETHistoryManager.m
@@ -0,0 +1,169 @@
+/*
+   ETHistoryManager.m
+
+   Copyright (C) 2008 Truls Becken <truls.becken@gmail.com>
+ 
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
+
+#import "ETHistoryManager.h"
+
+@implementation ETHistoryManager
+
+- (ETHistoryManager *) init
+{
+	[super init];
+	history = [[NSMutableArray alloc] init];
+	future = nil;
+	max_size = 100;
+	index = -1;
+	return self;
+}
+
+- (void) addObject: (id)object
+{
+	[self setFuture: nil];
+	if (max_size < 1 || index < max_size)
+	{
+		++index;
+	}
+	else
+	{
+		[history removeObjectAtIndex: 0];
+	}
+	[history addObject: object];
+}
+
+- (id) currentObject
+{
+	if (index < 0)
+	{
+		return nil;
+	}
+	return [history objectAtIndex: index];
+}
+
+- (void) next
+{
+	if ([self hasNext] == YES)
+	{
+		if (max_size < 1 || index < max_size)
+		{
+			++index;
+		}
+		else
+		{
+			[history removeObjectAtIndex: 0];
+		}
+	}
+}
+
+- (void) previous
+{
+	if (index > 0)
+	{
+		--index;
+	}
+}
+
+- (BOOL) hasNext
+{
+	if (index < (int)[history count] - 1)
+	{
+		return YES;
+	}
+
+	id object = [future nextObject];
+
+ 	if (object != nil)
+	{
+		[history addObject: object];
+		return YES;
+	}
+	else
+	{
+		DESTROY(future);
+		return NO;
+	}
+}
+
+- (BOOL) hasPrevious
+{
+	return index > 0;
+}
+
+- (id) peek: (int)relativeIndex
+{
+	int peekIndex = index + relativeIndex;
+
+	if (peekIndex < 0)
+	{
+		return nil;
+	}
+
+	for (int i = peekIndex - [history count] + 1; i > 0; i--)
+	{
+		id object = [future nextObject];
+
+		if (object != nil)
+		{
+			[history addObject: object];
+		}
+		else
+		{
+			DESTROY(future);
+			return nil;
+		}
+	}
+
+	return [history objectAtIndex: peekIndex];
+}
+
+- (void) clear
+{
+	[history removeAllObjects];
+	DESTROY(future);
+	index = -1;
+}
+
+- (void) setFuture: (NSEnumerator *)enumerator
+{
+	NSRange toEnd = NSMakeRange(index + 1, [history count]);
+	[history removeObjectsInRange: toEnd];
+	ASSIGN(future, enumerator);
+}
+
+- (void) setMaxHistorySize: (int)maxSize
+{
+	max_size = maxSize;
+}
+
+- (int) maxHistorySize
+{
+	return max_size;
+}
+
+- (void) dealloc
+{
+	DESTROY(history);
+	DESTROY(future);
+	[super dealloc];
+}
+
+@end
