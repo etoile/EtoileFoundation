81a52f87166992a6b1fab978aff110b6eccd03e0
Quentin Mathé <quentin.mathe@gmail.com>
2010-01-05 18:51:38 +0000

    Removed most of the old reflection API which has been superceded by the mirror-based one written by Eric. Update the remaining code to use the ObjC2 runtime API. Implemented -typeName.
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@5732 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/NSObject+Etoile.h
+++ Headers/NSObject+Etoile.h
@@ -1,151 +1,29 @@
-/*
-	NSObject+etoile.h
+/** <title>NSObject+Etoile</title>
 	
-	NSObject additions like basic metamodel.
+	<abstract>NSObject basic reflection additions.</abstract>
  
 	Copyright (C) 2007 Quentin Mathe
  
 	Author:  Quentin Mathe <qmathe@club-internet.fr>
 	Date:  August 2007
- 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License:  Modified BSD (see COPYING)
  */
 
 #import <Foundation/Foundation.h>
-#import <AppKit/AppKit.h>
-#ifndef GNUSTEP
-#import <objc/runtime.h>
-#endif
 
-/* Runtime Checks */
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5
-#define NEXT_RUNTIME_2
-#endif
-#ifndef NEXT_RUNTIME_2
-#define GNUSTEP_RUNTIME_COMPATIBILITY
-#endif
-#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-#import <GNUstepBase/GSObjCRuntime.h>
-#endif
+@class ETUTI;
 
-@class ETMethod, ETUTI;
+/** This category extends NSObject reflection API in a minimal way.
 
-/** Protocol which can be adopted by other object hierachy than NSObject rooted hierarchy */
-@protocol ETInspectableObject
-
-@end
-
-/** Utility metamodel for GNUstep/Cocoa Objective-C */
-
-@interface NSObject (Etoile) //<ETInspectableObject>
+The true Etoile reflection API is declared in ETReflection.h. */
+@interface NSObject (Etoile)
 
 + (NSArray *) allSubclasses;
 + (NSArray *) directSubclasses;
-
-/** Returns a object representing the receiver. Useful when sucblasses override
-    root class methods and make them unavailable to introspection. For example,
-	ETProtocol represents a protocol but overrides methods like -type, typeName
-	-name, -protocols and -protocolNames of NSObject, thereby you can know the 
-	properties of the represented protocol, but you cannot access the 
-	identically named properties which describes ETProtocol instance itself. */
-- (id) metaObject;
 
 - (ETUTI *) UTI;
 - (NSString *) typeName;
 + (NSString *) typePrefix;
 
-- (ETMethod *) methodForName: (NSString *)name;
-- (void) setMethod: (id)value forName: (NSString *)namme;
-
-- (NSArray *) protocolNames;
-- (NSArray *) protocols;
-
-- (NSArray *) methods;
-- (NSArray *) methodNames;
-/*- (NSArray *) instanceMethods;
-- (NSArray *) instanceMethodNames;
-- (NSArray *) classMethods;
-- (NSArray *) classMethodNames;
-
-- (void) addMethod: (ETMethod *)method;
-- (void) removeMethod: (ETMethod *)method;*/
-/** Method swizzling */
-/*- (void) replaceMethod: (ETMethod *)method byMethod: (ETMethod *)method;*/
-
-/** Low level methods used to implement method list edition */
-/*- (void) bindMethod: (ETMethod *) toSelector: (SEL)selector;
-- (void) bindSelector: (SEL) toMethod: (ETMethod *)method;*/
-
 @end
 
-
-@interface ETMethod : NSObject 
-{
-	@public
-#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	GSMethod _method;
-#endif
-}
-
-/*- (BOOL) isInstanceMethod;
-- (BOOL) isClassMethod;*/
-
-- (NSString *) name;
-- (SEL) selector;
-- (NSMethodSignature *) methodSignature;
-
-@end
-
-/** A Protocol counterpart for Foundation and NSObject root class */
-@interface ETProtocol : NSObject 
-{
-	@public
-	Protocol *_protocol;
-}
-
-+ (Protocol *) protocolForName: (NSString *)name;
-
-- (NSString *) name;
-- (ETUTI *) type;
-- (NSString *) typeName;
-
-/* Overriden NSObject methods to return eventual protocols adopted by the 
-   represented protcol */
-- (NSArray *) protocolNames;
-- (NSArray *) protocols;
-
-@end
-
-/**
- * Provides some introspection on Class objects.
- */
-@interface ETClass : NSObject
-/**
- * Returns an array of the Protocol objects which aClass explicitly conforms
- * to. (i.e., does not include protocols conformed to by aClass's superclasses
- * or protocols which the returned protocols conform to themselves.)
- */
-+ (NSArray *) protocolsForClass: (Class)aClass;
-@end
--- Source/NSObject+Etoile.m
+++ Source/NSObject+Etoile.m
@@ -1,122 +1,51 @@
 /*
-	NSObject+Etoile.m
-	
-	NSObject additions like basic metamodel.
- 
 	Copyright (C) 2007 Quentin Mathe
  
 	Author:  Quentin Mathe <qmathe@club-internet.fr>
 	Date:  August 2007
- 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	* Redistributions of source code must retain the above copyright notice,
-	  this list of conditions and the following disclaimer.
-	* Redistributions in binary form must reproduce the above copyright notice,
-	  this list of conditions and the following disclaimer in the documentation
-	  and/or other materials provided with the distribution.
-	* Neither the name of the Etoile project nor the names of its contributors
-	  may be used to endorse or promote products derived from this software
-	  without specific prior written permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-	THE POSSIBILITY OF SUCH DAMAGE.
+	License: Modified BSD (see COPYING)
  */
 
 #import "NSObject+Etoile.h"
-#import "EtoileCompatibility.h"
 #import "ETUTI.h"
 #import "Macros.h"
+#import "runtime.h"
+#import "EtoileCompatibility.h"
 
+/* Returns YES if subclass inherits directly or inherits from aClass.
 
-/** Returns the superclass of the class passed in parameter. */
-static inline Class ETGetSuperclass(Class aClass)
-{
-#if defined(GNU_RUNTIME)
-	if (CLS_ISRESOLV(aClass))
-	{
-		return aClass->super_class;
-	}
-	else
-	{
-		if (aClass->super_class != NULL)
-		{
-			return objc_lookup_class((char*)aClass->super_class);
-		}
-		else
-		{
-			return Nil;
-		}
-	}
-#elif defined(GNUSTEP_RUNTIME_COMPATIBILITY)
-	return GSObjCSuper(aClass);
-#elif defined(NEXT_RUNTIME_2)
-	return class_getSuperclass(aClass);
-#else /* NEXT_RUNTIME 1 */
-	return aClass->superclass;
-#endif
-}
-
-/** Returns YES if subclass inherits directly or inherits from aClass.
-	Unlike +[NSObject isSubclassOfClass:] returns no if subclass and aClass are 
-	equal. */
+Unlike +[NSObject isSubclassOfClass:] returns no if subclass and aClass are 
+equal. */
 static inline BOOL ETIsSubclassOfClass(Class subclass, Class aClass)
 {
 	Class parentClass = subclass;
 
 	while (parentClass != nil)
 	{
-		parentClass = ETGetSuperclass(parentClass);
+		parentClass = class_getSuperclass(parentClass);
 		if (parentClass == aClass)
 			return YES;
-    } 
+	}
 
 	return NO;
 }
 
-@implementation NSObject (Etoile) //<ETInspectableObject>
+@implementation NSObject (Etoile)
 
 /** Returns all descendant subclasses of the receiver class. 
-    The returned array doesn't include the receiver class. */
+
+The returned array doesn't include the receiver class.
+
+You should rather use -[ETClassMirror allSubclassMirrors], this method could 
+be deprecated in the future. */
 + (NSArray *) allSubclasses
 {
-	#if 0
-	//#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	/* Fast because it uses the sibling class facility of GNU runtime
-	   NOTE: Not used, because it is broken for classes that have not yet 
-	   received their first message. */
-	return GSObjCAllSubclassesOfClass(self);
-
-	#elif defined(GNU_RUNTIME)
-	NSMutableArray *subclasses = [NSMutableArray arrayWithCapacity: 300];
-	void *state = NULL;
-	Class nextClass = Nil;
-	while(Nil != (nextClass = objc_next_class(&state)))
-	{
-		if (ETIsSubclassOfClass(nextClass, self))
-		 {
-			[subclasses addObject: nextClass];
-		}
-	}
-	return subclasses;
-
-	#else /* NEXT_RUNTIME and NEXT_RUNTIME_2 */	
+	// NOTE: The sibling class facility of GNU runtime would eventually be 
+	// faster (see GSObjCAllSubclassesOfClass as an example), however it 
+	// doesn't work for classes that have not yet received their first message.
 	NSMutableArray *subclasses = [NSMutableArray arrayWithCapacity: 300];
 	Class *allClasses = NULL;
-	int numberOfClasses;
-
-	allClasses = NULL;
-	numberOfClasses = objc_getClassList(NULL, 0);
+	int numberOfClasses = objc_getClassList(NULL, 0);
 
 	if (numberOfClasses > 0)
 	{
@@ -133,25 +62,20 @@ static inline BOOL ETIsSubclassOfClass(Class subclass, Class aClass)
 	}
 
 	return subclasses;
-	#endif
 }
 
 /** Returns all subclasses which inherit directly from the receiver class. 
-    Subclasses that belongs to the class hierarchy of the receiver class but 
-	whose superclasses aren't equal to it, are excluded.
-    The returned array doesn't include the receiver class. */
+
+Subclasses that belongs to the class hierarchy of the receiver class but whose 
+superclasses aren't equal to it, are excluded.
+
+The returned array doesn't include the receiver class. */
 + (NSArray *) directSubclasses
 {
-	#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	return GSObjCDirectSubclassesOfClass(self);
-
-	#else /* NEXT_RUNTIME and NEXT_RUNTIME_2 */
+	/* See also the note in +allSubclasses */
 	NSMutableArray *subclasses = [NSMutableArray arrayWithCapacity: 30];
 	Class *allClasses = NULL;
-	int numberOfClasses;
-	 
-	allClasses = NULL;
-	numberOfClasses = objc_getClassList(NULL, 0);
+	int numberOfClasses = objc_getClassList(NULL, 0);
 	 
 	if (numberOfClasses > 0)
 	{
@@ -159,7 +83,7 @@ static inline BOOL ETIsSubclassOfClass(Class subclass, Class aClass)
 		numberOfClasses = objc_getClassList(allClasses, numberOfClasses);
 		for (int i = 0; i < numberOfClasses; i++)
 		{
-			if (ETGetSuperclass(allClasses[i]) == self)
+			if (class_getSuperclass(allClasses[i]) == self)
 			{
 				[subclasses addObject: allClasses[i]];
 			}
@@ -168,268 +92,51 @@ static inline BOOL ETIsSubclassOfClass(Class subclass, Class aClass)
 	}
 	
 	return subclasses;
-	#endif
 }
 
-/** Returns a object representing the receiver. Useful when sucblasses override
-    root class methods and make them unavailable to introspection. For example,
-	ETProtocol represents a protocol but overrides methods like -type, typeName
-	-name, -protocols and -protocolNames of NSObject, thereby you can know the 
-	properties of the represented protocol, but you cannot access the 
-	identically named properties which describes ETProtocol instance itself. */
-- (id) metaObject
-{
-	return nil;
-}
+/** Returns the uniform type identifier of the object. 
 
-/** Returns the uniform type identifier of the object. The UTI object encodes 
-	the type of the object in term of namespaces and multiple inheritance. 
-	By default, the UTI object is shared by all instances by being built from 
-	the class name. If you need to introduce type at instance level, you can
-	do it by overriding this method. */
+The UTI object encodes the type of the object in term of namespaces and 
+multiple inheritance. 
+
+By default, the UTI object is shared by all instances by being built from the 
+class name. If you need to introduce type at instance level, you can do it by 
+overriding this method. */
 - (ETUTI *) UTI
 {
 	return [ETUTI typeWithClass: [self class]];
 }
 
-/** Returns the type name which is the last component of type string returned 
-	by the UTI object. This type name doesn't include the class prefix.
-	This method is a shortcut for [[self UTI] typeName]. */
+/** Returns the type name which is the last component of the string value returned 
+by the receiver UTI object.
+
+See also -UTI. */
 - (NSString *) typeName
 {
-	return nil;
+	NSString *utiString = [[self UTI] stringValue];
+	NSRange range = [utiString rangeOfString: @"." options: NSBackwardsSearch];
+	
+	if (NSNotFound == range.location || 1 != range.length)
+	{
+		return @"";
+	}
+
+	return [utiString substringFromIndex: range.location + 1];
 }
 
-/** Returns the type prefix, usually the prefix part of the type name returned
-	by -className.
-	You must override this method in your subclass to indicate the prefix of 
-	your new class name. Take note the prefix will logically apply to every 
-	subsequent subclasses inheriting from the class that redefines 
-	-typePrefix. */
+/** <override-dummy />
+Returns the type prefix, usually the prefix part of the type name returned
+by -className.
+
+By default, returns 'NS'.
+
+You must override this method in your subclass to indicate the prefix of your 
+new class name. Take note the prefix will logically apply to every subsequent 
+subclasses inheriting from the class that redefines -typePrefix. */
 + (NSString *) typePrefix
 {
 	return @"NS";
 }
 
-- (ETMethod *) methodForName: (NSString *)name
-{
-	// BOOL searchInstanceMethods, BOOL searchSuperClasses
-	ETMethod *methodObject = [[ETMethod alloc] init];
-	
-	#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	GSMethod method = GSGetMethod([self class], NSSelectorFromString(name), YES, YES);
-	methodObject->_method = method;
-	#else
-	
-	#endif
-	
-	return AUTORELEASE(methodObject);
-}
-
-- (void) setMethod: (id)value forName: (NSString *)name
-{
-
-}
-
-- (NSArray *) protocolNames
-{
-	NSMutableArray *protocolNames = [NSMutableArray array];
-	FOREACH([self protocols], protocol, Protocol *)
-	{
-		[protocolNames addObject: [NSString stringWithUTF8String: [protocol name]]];
-	}
-	return protocolNames;
-}
-
-// FIXME: Not sure if this is the right interpretation for -protocols,
-//        currently it returns all protocols explicitly conformed to by the
-//        object's class, or its superclass, or superclasses's class, etc.,
-//        but not the ancestor protocols of those protocols.
-//        
-//        We also need a -allProtocols method which adds all the ancestors 
-//        to the protocols returned by this method.
-- (NSArray *) protocols
-{
-	#if defined(GNU_RUNTIME)
-	NSMutableArray *protocols = [NSMutableArray array];
-
-	Class class = [self class];
-	while (YES)
-	{
-		[protocols addObjectsFromArray: [ETClass protocolsForClass: class]];
-		if (class == [NSObject class])
-			break;
-		class = ETGetSuperclass(class);
-	}
-
-	// FIXME: Return immutable array
-	return protocols;
-	#else
-	return nil;
-	#endif
-}
-
-- (NSArray *) methods
-{
-	NSMutableArray *methods = [NSMutableArray array];
-	NSEnumerator *e = [[self methodNames] objectEnumerator];
-	NSString *methodName = nil;
-	
-	while ((methodName = [e nextObject]) != nil)
-	{
-		[methods addObject: [self methodForName: methodName]];
-	}
-	
-	// FIXME: Return immutable array
-	return methods;
-}
-
-- (NSArray *) methodNames
-{
-	#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	return GSObjCMethodNames(self);
-	#else
-	return nil;
-	#endif
-}
-
-#if 0
-- (NSArray *) instanceMethods;
-- (NSArray *) instanceMethodNames;
-- (NSArray *) classMethods;
-- (NSArray *) classMethodNames;
-
-- (void) addMethod: (ETMethod *)method;
-- (void) removeMethod: (ETMethod *)method;
-/** Method swizzling */
-- (void) replaceMethod: (ETMethod *)method byMethod: (ETMethod *)method;
-
-/** Low level methods used to implement method list edition */
-- (void) bindMethod: (ETMethod *) toSelector: (SEL)selector;
-- (void) bindSelector: (SEL) toMethod: (ETMethod *)method;
-#endif
-
 @end
 
-@implementation ETMethod 
-
-/*- (BOOL) isInstanceMethod
-{
-	
-}
-
-- (BOOL) isClassMethod
-{
-
-}*/
-
-- (NSString *) name
-{
-	return NSStringFromSelector([self selector]);
-}
-
-- (SEL) selector
-{
-	#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	return _method->method_name;
-	#else
-	return NULL;
-	#endif
-}
-
-- (NSMethodSignature *) methodSignature
-{
-	// FIXME: Build sig with member char *method_types of GSMethod
-	return nil;
-}
-
-@end
-
-/** A Protocol counterpart for Foundation and NSObject root class */
-@implementation ETProtocol
-
-/** Returns an ObjC runtime protocol object for the given protocol name. */
-+ (Protocol *) protocolForName: (NSString *)name
-{
-#ifdef GNUSTEP_RUNTIME_COMPATIBILITY
-	return GSProtocolFromName([name UTF8String]);
-#else
-	return nil;
-#endif
-}
-
-- (NSString *) name
-{
-	return nil;
-	//return [NSString stringWithCString: [_protocol name]];
-}
-
-- (ETUTI *) type
-{
-	return nil;
-}
-
-- (NSString *) typeName
-{
-	return [self name];
-}
-
-// FIXME: Add methods like -allAncestorProtocols -allAncestorProtocolNames
-
-/* Overriden NSObject methods to return eventual protocols adopted by the 
-   represented protcol */
-- (NSArray *) protocolNames
-{	
-	//return [[self protocols] valueForKey: @"name"];
-	return nil;
-}
-
-- (NSArray *) protocols
-{
-	/*Class pClass = [_protocol class];
-	struct objc_protocol_list pIterator = pClass->objc_protocols;
-	NSMutableArray *protocols = [NSMutableArray array];
-	
-	do 
-	{
-		Protocol *p = (Protocol *)pIterator->List;
-		ETProtocol *protocol = [[ETProtocol alloc] init];
-		
-		protocol->_protocol = p;
-		[protocols addObject: protocol];
-		RELEASE(protocol)
-		pIterator = pIterator->next;
-		
-	} while (pInterator->next != NULL)
-
-	return AUTORELEASE(protocols);*/
-	return nil;
-}
-
-@end
-
-@implementation ETClass
-+ (NSArray *) protocolsForClass: (Class)aClass
-{
-#if defined(GNU_RUNTIME)
-	if (aClass == Nil)
-	{
-		return nil;
-	}
-	NSMutableArray *protocols = [NSMutableArray array];
-
-	for (struct objc_protocol_list* iter = aClass->protocols; iter != NULL; iter = iter->next)
-	{
-		for (size_t i = 0; i < iter->count; i++)
-		{
-			Protocol *protocol = iter->list[i];
-			[protocols addObject: protocol];
-		}
-	}
-	return protocols;
-#else
-#warning +protocolsForClass not supported on your ObjC runtime.
-	return nil;
-#endif
-}
-@end
