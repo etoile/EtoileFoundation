cad6919cae296f09a77e2927c31da9a8f4389f65
Quentin Mathé <quentin.mathe@gmail.com>
2011-05-01 19:08:33 +0000

    Reformatted the doc in ETEntityDescription to match the other metamodel classes
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@6907 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/ETEntityDescription.h
+++ Headers/ETEntityDescription.h
@@ -15,11 +15,9 @@
 
 @class ETPackageDescription, ETPropertyDescription, ETValidationResult, ETUTI;
 
-/**
- * @group Model and Metamodel
- *
- * A description of an entity, which can either be a class or a prototype.
- */
+/** @group Model and Metamodel
+
+A description of an entity, which can either be a class or a prototype. */
 @interface ETEntityDescription : ETModelElementDescription <ETCollection, ETCollectionMutation>
 {
 	@private
@@ -30,15 +28,13 @@
 }
 /** Self-description (aka meta-metamodel). */
 + (ETEntityDescription *) newEntityDescription;
-/**
- * The name of the entity description that should end the parent chain of every 
- * entity description.
- *
- * This entity description is the Object primitive in the repository. 
- * See ETModelDescriptionRepository.
- *
- * Will be used by -checkConstraints:.
- */
+/** The name of the entity description that should end the parent chain of 
+every entity description.
+
+This entity description is the Object primitive in the repository. See 
+ETModelDescriptionRepository.
+
+Will be used by -checkConstraints:. */
 + (NSString *) rootEntityDescriptionName;
 
 /** @taskunit Querying Type */
@@ -47,101 +43,67 @@
 - (BOOL) isEntityDescription;
 /**  Returns <em>Entity</em>. */
 - (NSString *) typeDescription;
-/**
- * Whether or not this entity is a primitive (i.e. describes attributes and not 
- * relationships)
- *
- * Primitives include both object and C primitives. e.g. NSString, NSDate, 
- * NSInteger, float, etc.
- *
- * See also -[ETPropertyDescription isRelationship].
- */
+/** Whether or not this entity is a primitive (i.e. describes attributes and 
+not relationships).
+
+Primitives include both object and C primitives. e.g. NSString, NSDate, 
+NSInteger, float, etc.
+
+See also -[ETPropertyDescription isRelationship]. */
 - (BOOL) isPrimitive;
-/**
- * Whether or not this entity is a C primitive (i.e. describes attributes whose 
- * values are not objects). e.g. NSInteger, float, etc.
- *
- * If YES is returned, -isPrimitive returns the same.
- *
- * See also -[ETPropertyDescription isPrimitive].
- */
+/** Whether or not this entity is a C primitive (i.e. describes attributes whose 
+values are not objects). e.g. NSInteger, float, etc.
+
+If YES is returned, -isPrimitive returns the same.
+
+See also -[ETPropertyDescription isPrimitive]. */
 - (BOOL) isCPrimitive;
 
 /** @taskunit Model Specification */
 
-/**
- * Whether or not this entity is abstract (i.e. can't be instantiated).
- */
+/** Whether or not this entity is abstract (i.e. can't be instantiated). */
 - (BOOL) isAbstract;
-/**
- * Whether or not this entity is abstract (i.e. can't be instantiated).
- */
+/** Whether or not this entity is abstract (i.e. can't be instantiated). */
 - (void) setAbstract: (BOOL)isAbstract;
 
 /** @taskunit Inheritance and Owning Package */
 
-/**
- * Whether this is a root entity (has no parent entity).
- */
+/** Whether this is a root entity (has no parent entity). */
 - (BOOL) isRoot;
-/**
- * The parent entity of this entity. (Superclass or prototype)
- */
+/** The parent entity of this entity. (Superclass or prototype) */
 - (ETEntityDescription *) parent;
-/**
- * The parent entity of this entity. (Superclass or prototype)
- */
+/** The parent entity of this entity. (Superclass or prototype) */
 - (void) setParent: (ETEntityDescription *)parentDescription;
-/** 
- * The package to which this entity belongs to.
- */
+/** The package to which this entity belongs to. */
 - (ETPackageDescription *) owner;
-/** 
- * The package to which this entity belongs to.
- */
+/** The package to which this entity belongs to. */
 - (void) setOwner: (ETPackageDescription *)owner;
 
 /** @taskunit Property Descriptions */
 
-/**
- * Names of the property descriptions (not including those declared in parent 
- * entities).
- */
+/** Names of the property descriptions (not including those declared in parent 
+entities). */
 - (NSArray *) propertyDescriptionNames;
-/**
- * Names of all property descriptions including those declared in parent 
- * entities.
- */
+/** Names of all property descriptions including those declared in parent 
+entities. */
 - (NSArray *) allPropertyDescriptionNames;
-/**
- * Descriptions of the properties declared on this entity (not including those
- * declared in parent entities).
- */
+/** Descriptions of the properties declared on this entity (not including those
+declared in parent entities). */
 - (NSArray *) propertyDescriptions;
-/**
- * Descriptions of the properties declared on this entity (not including those
- * declared in parent entities).
- */
+/** Descriptions of the properties declared on this entity (not including those 
+declared in parent entities). */
 - (void) setPropertyDescriptions: (NSArray *)propertyDescriptions;
-/** 
- * Adds the given property description to this entity. 
- */
+/** Adds the given property description to this entity. */
 - (void) addPropertyDescription: (ETPropertyDescription *)propertyDescription;
-/**
- * Removes the given property description from this entity. 
- */
+/** Removes the given property description from this entity. */
 - (void) removePropertyDescription: (ETPropertyDescription *)propertyDescription;
-/**
- * Descriptions of the entity's properties, including those declared in parent
- * entities.
- */
+/** Descriptions of the entity's properties, including those declared in parent 
+entities. */
 - (NSArray *) allPropertyDescriptions;
-/** 
- * Returns the property description which matches the given name.
- *
- * See also -[ETModelElementDescription name] which is inherited by 
- * ETPropertyDescription.
- */
+/** Returns the property description which matches the given name.
+
+See also -[ETModelElementDescription name] which is inherited by 
+ETPropertyDescription. */
 - (ETPropertyDescription *)propertyDescriptionForName: (NSString *)name;
 
 /** @taskunit Validation */
