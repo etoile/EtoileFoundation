818d871cd07111a6105eaef9f9cea3201ef92e86
Quentin Mathé <quentin.mathe@gmail.com>
2011-09-02 10:41:04 +0000

    Added NSBlocks.m to the Xcode project and tweaked block additions to work on Mac OS X. All tests pass now.
    The block class name is _NSBlock on GNUstep but NSBlock on Mac OS X.
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@7282 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/NSBlocks.m
+++ Source/NSBlocks.m
@@ -1,5 +1,11 @@
 #import <Foundation/NSObject.h>
+#ifdef GNUSTEP
 #import "objc/blocks_runtime.h"
+#define BLOCK_CLASS_NAME "_NSBlock"
+#else
+#import <objc/runtime.h>
+#define BLOCK_CLASS_NAME "NSBlock"
+#endif
 
 // Define __has_feature() for compilers that don't support it.
 #ifndef __has_feature
@@ -18,7 +24,7 @@
 	unsigned int methodCount;
 	Method *methods = 
 		class_copyMethodList(objc_getClass("ETBlock"), &methodCount);
-	id blockClass = objc_lookUpClass("_NSBlock");
+	id blockClass = objc_lookUpClass(BLOCK_CLASS_NAME);
 	for (Method *m = methods ; NULL!=*m ; m++)
 	{
 		class_addMethod(blockClass, method_getName(*m),
