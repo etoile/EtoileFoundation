ee9d248092d457dca57442b7833051eacc10b985
Eric Wasylishen <ewasylishen@gmail.com>
2013-07-04 15:46:44 +0000

    EtoileFoundation: Macros.h: add safer D(), A(), and S() macros; add INDEXSET() macro
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/EtoileFoundation@8567 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Headers/Macros.h
+++ Headers/Macros.h
@@ -137,11 +137,49 @@ while(enumerator != nil && (object = next ## object ## in ## enumerator(\
 #endif
 
 /** Shortcut macro to create a NSDictionary. Same as +[NSDictionary dictionaryWithObjectsAndKeys:]. */
-#define D(...) [NSDictionary dictionaryWithObjectsAndKeys:__VA_ARGS__ , nil]
+#define D(...) ({ \
+    id __objects_and_keys[] = {__VA_ARGS__}; \
+    size_t __objects_and_keys_count = sizeof(__objects_and_keys) / sizeof(id); \
+    if ((__objects_and_keys_count % 2) != 0) \
+    { \
+	    [NSException raise: NSInvalidArgumentException \
+					format: @"D() macro expects an even number of arguments!"]; \
+    } \
+    size_t __objects_count = __objects_and_keys_count / 2; \
+    id __objects[__objects_count]; \
+    id __keys[__objects_count]; \
+    size_t __objects_iterator; \
+    for (__objects_iterator = 0; __objects_iterator < __objects_count; __objects_iterator++) \
+    { \
+        __objects[__objects_iterator] = __objects_and_keys[2 * __objects_iterator]; \
+        __keys[__objects_iterator] = __objects_and_keys[(2 * __objects_iterator) + 1]; \
+    } \
+    [NSDictionary dictionaryWithObjects: __objects forKeys: __keys count: __objects_count]; \
+})
 /** Shortcut macro to create a NSArray. Same as +[NSArray dictionaryWithObjects:]. */
-#define A(...) [NSArray arrayWithObjects:__VA_ARGS__ , nil]
+#define A(...) ({ \
+    id __objects[] = {__VA_ARGS__}; \
+    [NSArray arrayWithObjects: __objects \
+						count: (sizeof(__objects)/sizeof(id))]; \
+})
 /** Shortcut macro to create a NSSet. Same as +[NSSet setWithObjects:]. */
-#define S(...) [NSSet setWithObjects:__VA_ARGS__ , nil]
+#define S(...) ({ \
+    id __objects[] = {__VA_ARGS__}; \
+    [NSSet setWithObjects: __objects \
+					count: (sizeof(__objects)/sizeof(id))]; \
+})
+/** Shortcut macro to create a NSIndexSet. */
+#define INDEXSET(...) ({ \
+	NSMutableIndexSet *__result = [NSMutableIndexSet indexSet]; \
+	const NSUInteger __indices[] = {__VA_ARGS__}; \
+    const size_t __indices_count = sizeof(__indices) / sizeof(NSUInteger); \
+	size_t __indices_iterator; \
+	for (__indices_iterator = 0; __indices_iterator < __indices_count; __indices_iterator++) \
+	{ \
+		[__result addIndex: __indices[__indices_iterator]]; \
+	} \
+	[[[NSIndexSet alloc] initWithIndexSet: __result] autorelease]; \
+})
 
 #ifdef DEFINE_STRINGS
 #define EMIT_STRING(x) NSString *x = @"" # x;
