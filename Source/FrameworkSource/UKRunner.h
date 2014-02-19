18b66f21c3ad8af1743c3ca3c7d54264cedab031
Quentin Mathé <quentin.mathe@gmail.com>
2014-02-19 15:30:10 +0000

    * Documented most UKRunner API
    * Added a Xcode project shell script to ouput FIXME and TODO on compilation
    
    
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/UnitKit@10354 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Source/FrameworkSource/UKRunner.h
+++ Source/FrameworkSource/UKRunner.h
@@ -26,40 +26,105 @@
 
 @interface UKRunner : NSObject
 {
+	@private
 	NSMutableSet *setUpClasses;
-    int testClassesRun;
-    int testMethodsRun;
+	int testClassesRun;
+	int testMethodsRun;
 }
 
-+ (int) runTests;
+/** @taskunit Tool Support */
 
-- (NSArray *) parseArgumentsWithCurrentDirectory: (NSString *)cwd;
-- (int) reportTestResults;
+/**
+ * Creates a new runner and uses it to run the tests based on the command-line
+ * arguments.
+ *
+ * For all the test bundles collected (or provided as arguments), this method
+ * uses -runTests:inBundleAtPath:currrentDirectory: to run the tests contained
+ * in each one. When all test bundles have been run, -reportTestResults is
+ * called to output the combined results.
+ *
+ * ukrun main() creates an autorelease pool, and uses this method to run the
+ * tests.
+ */
++ (int)runTests;
+/**
+ * Loads the given test bundle, and runs all its tests.
+ *
+ * If the bundle path is not an absolute path, the method searches the test
+ * bundles to load in the given directory (the current directory, when +runTests
+ * is used).
+ */
+- (void)runTests: (NSArray *)testClasses
+  inBundleAtPath: (NSString *)bundlePath
+currentDirectory: (NSString *)cwd;
+/**
+ * Runs all the tests in the given test bundle.
+ *
+ * If the bundle is nil, a principal class can still be provided in argument.
+ *
+ * For a valid bundle, the principal class argument is ignored.
+ *
+ * For test related configuration, +willRunTestSuite and +didRunTestSuite
+ * are sent to the principal class (see UKPrincipalClassNotifications).
+ */
+- (void)runTestsInBundle: (NSBundle *)bundle
+          principalClass: (Class)principalClass;
+/**
+ * Runs all the tests in the tested classes in the given test bundle.
+ *
+ * This method behaves the same than -runTestsInBundle:principalClass:.
+ *
+ * If testedClasses is nil, then it is the same than passing all the test
+ * classes present in the bundle.
+ */
+- (void)runTests: (NSArray *)testedClasses
+        inBundle: (NSBundle *)bundle
+  principalClass: (Class)principalClass;
 
-- (void) runTestsInBundle: (NSBundle *)bundle principalClass: (Class)principalClass;
-- (void) runTests: (NSArray*)testClasses 
-   inBundleAtPath: (NSString *)bundlePath 
- currentDirectory: (NSString *)cwd;
+
+/** @taskunit Running Tests */
+
+
 - (void)runTestsInClass: (Class)testClass;
-- (void) runTests: (NSArray *)testMethods onInstance: (BOOL)instance ofClass:(Class)testClass;
+- (void)runTests: (NSArray *)testMethods
+      onInstance: (BOOL)instance
+		 ofClass: (Class)testClass;
 
-- (BOOL) setUpAppObjectIfNeededForBundle: (NSBundle *)testBundle;
 
-#ifndef GNUSTEP
-+ (void) performGrowlNotification
-:(int) testsPassed 
-:(int) testsFailed
-:(int) exceptionsReported
-:(int) testClassesRun
-:(int) testMethodsRun;
-#endif
+/** @taskunit Test Reporting */
+
+
+/**
+ * Logs a summary that reports the current run results:
+ *
+ * <list>
+ * <item>how many test classes and test methods were executed</item>
+ * <item>how many tests failed and passed</item>
+ * <item>how many uncaught exceptions occurred</item>
+ * </list>
+ *
+ * If no tests failed and no uncaught exceptions occured, returns 0 to indicate
+ * success, otherwise returns -1.
+ */
+- (int)reportTestResults;
 
 @end
 
 @interface NSObject (UKPrincipalClassNotifications)
-+ (void) willRunTestSuite;
-+ (void) didRunTestSuite;
++ (void)willRunTestSuite;
++ (void)didRunTestSuite;
 @end
 
-NSArray *UKTestClasseNamesFromBundle(NSBundle *bundle);
+/**
+ * Returns all the test classes present in the given bundle, and sorted by name.
+ *
+ * To be a test class, a class (or its superclass) must conform to UKTest
+ * protocol.
+ */
+NSArray *UKTestClasseNamesFromBundle (NSBundle *bundle);
+/**
+ * Returns all the test method names sorted by name, for the given class.
+ *
+ * All the superclass methods are collected (until the root class is reached).
+ */
 NSArray *UKTestMethodNamesFromClass(Class c);
