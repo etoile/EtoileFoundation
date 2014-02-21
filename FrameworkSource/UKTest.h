c559cc1382726e08e92a884dd51f251a8e8f1f4d
Quentin Mathé <quentin.mathe@gmail.com>
2014-02-21 18:05:43 +0000

    Documented most of UKTest
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/UnitKit@10389 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- FrameworkSource/UKTest.h
+++ FrameworkSource/UKTest.h
@@ -21,61 +21,134 @@
 
 #import <Foundation/NSObject.h>
 
-/*!
- @protocol UKTest
- @abstract The marker protocol that indicates that a class should be picked up by a system.
- @discussion (description)
-*/
+/**
+ * @abstract The protocol that marks a class as a test class.
+ *
+ * All classes that conforms to UKTest, including their subclasses, are picked 
+ * up by UKRunner.
+ *
+ * If a filtering option such as -c is passed to <em>ukrun</em>, this can 
+ * prevent the a test class to be picked up.
+ */
 @protocol UKTest
 
 @end
 
-#define UKPass() [[UKTestHandler handler] passInFile:__FILE__ line:__LINE__]
-
-#define UKFail() [[UKTestHandler handler] failInFile:__FILE__ line:__LINE__]
-
-#define UKTrue(condition) [[UKTestHandler handler] testTrue:(condition) inFile:__FILE__ line:__LINE__]
-
-#define UKFalse(condition) [[UKTestHandler handler] testFalse:(condition) inFile:__FILE__ line:__LINE__]
-
-#define UKNil(ref) [[UKTestHandler handler] testNil: (void *)(ref) inFile:__FILE__ line:__LINE__] 
-
-#define UKNotNil(ref) [[UKTestHandler handler] testNotNil: (void *)(ref) inFile:__FILE__ line:__LINE__]
-
-#define UKIntsEqual(a, b) [[UKTestHandler handler] testInt:(a) equalTo:(b) inFile:__FILE__ line:__LINE__]
-
-#define UKIntsNotEqual(a, b) [[UKTestHandler handler] testInt:(a) notEqualTo:(b) inFile:__FILE__ line:__LINE__]
-
-#define UKFloatsEqual(a, b, d) [[UKTestHandler handler] testFloat:(a) equalTo:(b) delta:(d) inFile:__FILE__ line:__LINE__]
-
-#define UKFloatsNotEqual(a, b, d) [[UKTestHandler handler] testFloat:(a) notEqualTo:(b) delta:(d) inFile:__FILE__ line:__LINE__]
-
 /**
- * UKObjectKindOf(a, b) test macro that uses -isKindOfClass: behind the scene. 
+ * Reports a success.
+ */
+#define UKPass() [[UKTestHandler handler] passInFile:__FILE__ line:__LINE__]
+/**
+ * Reports a failure.
+ */
+#define UKFail() [[UKTestHandler handler] failInFile:__FILE__ line:__LINE__]
+/**
+ * Tests that an expression is true.
+ */
+#define UKTrue(condition) [[UKTestHandler handler] testTrue:(condition) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that an expression is false.
+ */
+#define UKFalse(condition) [[UKTestHandler handler] testFalse:(condition) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that <code>ref == nil</code>.
+ */
+#define UKNil(ref) [[UKTestHandler handler] testNil: (void *)(ref) inFile:__FILE__ line:__LINE__] 
+/**
+ * Tests that <code>ref != nil</code>.
+ */
+#define UKNotNil(ref) [[UKTestHandler handler] testNotNil: (void *)(ref) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that two primitive integers are equal.
  *
- * Most of the time UKObjectsEqual([a class], [b class]) would be similar, but 
- * not always (i.e. NSCFArray/NSArray on Mac OS X). Example:
+ * a is the expected value and b the tested value.
+ */
+#define UKIntsEqual(a, b) [[UKTestHandler handler] testInt:(a) equalTo:(b) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that two primitive integers are not equal.
+ *
+ * a is the non-expected value and b the tested value.
+ */
+#define UKIntsNotEqual(a, b) [[UKTestHandler handler] testInt:(a) notEqualTo:(b) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that two primitive floats are equal or almost, this evaluates whether 
+ * <code>fabs(a - b) <= d</code> is true.
+ *
+ * d is the error margin.
+ *
+ * a is the expected value and b the tested value.
+ */
+#define UKFloatsEqual(a, b, d) [[UKTestHandler handler] testFloat:(a) equalTo:(b) delta:(d) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests that two primitive floats are not equal, this evaluates whether 
+ * <code>fabs(a - b) > d</code> is true. 
+ *
+ * d is the error margin.
+ *
+ * a is the non-expected value and b the tested value.
+ */
+#define UKFloatsNotEqual(a, b, d) [[UKTestHandler handler] testFloat:(a) notEqualTo:(b) delta:(d) inFile:__FILE__ line:__LINE__]
+/**
+ * Tests macro that a is a subclass of b, this uses -isKindOfClass: behind the 
+ * scene. 
+ *
+ * Most of the time <code>UKObjectsEqual([a class], [b class])</code> would be 
+ * similar, but not always (i.e. NSCFArray/NSArray on Mac OS X). Example:
  *
  * <example>
  * UKObjectKindOf(myObject, NSArray)
  * </example>
  */
 #define UKObjectKindOf(a, b) [[UKTestHandler handler] testObject:(a) kindOf:[b class] inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that <code>[a isEqual: b]</code>.
+ *
+ * a is the expected value and b the tested value.
+ */
 #define UKObjectsEqual(a, b) [[UKTestHandler handler] testObject:(a) equalTo:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that <code>![a isEqual: b]</code>.
+ *
+ * a is the non-expected value and b the tested value.
+ */
 #define UKObjectsNotEqual(a, b) [[UKTestHandler handler] testObject:(a) notEqualTo:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that the objects are identical with <code>a == b</code>.
+ *
+ * a is the expected value and b the tested value.
+ */
 #define UKObjectsSame(a, b) [[UKTestHandler handler] testObject:(a) sameAs:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that the objects are not identical with a != b.
+ *
+ * a is the non-expected value and b the tested value.
+ */
 #define UKObjectsNotSame(a, b) [[UKTestHandler handler] testObject:(a) notSameAs:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that <code>[a isEqual: b]</code>.
+ *
+ * a is the expected value and b the tested value.
+ *
+ * This is the same than UKObjectsEqual(), this just helps readibility a bit, 
+ * since testing string equality is pretty common.
+ */
 #define UKStringsEqual(a, b) [[UKTestHandler handler] testString:(a) equalTo:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that <code>![a isEqual: b]</code>.
+ *
+ * a is the non-expected value and b the tested value.
+ *
+ * This is the same than UKObjectsNotEqual(), this just helps readibility a bit, 
+ * since testing string equality is pretty common.
+ */
 #define UKStringsNotEqual(a, b) [[UKTestHandler handler] testString:(a) notEqualTo:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that b is a substring of a, this uses -[NSString rangeOfString:].
+ */
 #define UKStringContains(a, b) [[UKTestHandler handler] testString:(a) contains:(b) inFile:__FILE__ line:__LINE__]
-
+/**
+ * Tests that b is not a substring of a, this uses -[NSString rangeOfString:].
+ */
 #define UKStringDoesNotContain(a, b) [[UKTestHandler handler] testString:(a) doesNotContain:(b) inFile:__FILE__ line:__LINE__]
 
 /*
