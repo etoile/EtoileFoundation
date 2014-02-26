36ccdf79601faa1c41fd9d111fe1fffb5841a39f
Quentin Mathé <quentin.mathe@gmail.com>
2014-02-26 14:17:32 +0000

    Added Travis test build support to UnitKit
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/UnitKit@10453 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ test-macosx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This is used in the "Run Script" phase of each of the targets we build,
+# if it is set to 1, the resulting binary is run as part of the xcodebuild
+export TEST_AUTORUN=1 
+
+xcodebuild -project UnitKit.xcodeproj -scheme TestUnitKit
+teststatus=$?
+
+# printstatus 'message' status
+function printstatus {
+  if [[ $2 == 0 ]]; then
+    echo "(PASS) $1"
+  else
+    echo "(FAIL) $1"
+  fi
+}
+
+echo "UnitKit Tests Summary"
+echo "====================="
+printstatus UnitKit $teststatus
+
+exitstatus=$(( teststatus ))
+exit $exitstatus
